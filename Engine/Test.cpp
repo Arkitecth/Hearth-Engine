697553e9211eef4c013464330014c093c946d0d0
Trent_T <trenttuedon2000@gmail.com>
2026-07-13 13:16:31 -0400

    Add Test Class

---
+++ Engine/Test.cpp
@@ -0,0 +1,7 @@
+#include "Test.h"
+#include <iostream>
+
+void HTE::Test::print() 
+{
+	std::cout << "Hello From Dynamic Library" << '\n';
+}
---
+++ Engine/Test.h
@@ -0,0 +1,8 @@
+namespace HTE 
+{
+	class Test 
+	{
+		public:
+			void print(); 
+	}; 
+} 
