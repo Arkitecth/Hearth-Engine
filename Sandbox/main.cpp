34d07166aa84222f14a22228a5716d779d0b6523
Trent_T <trenttuedon2000@gmail.com>
2026-07-13 18:13:59 -0400

    Add logging with ascii colors

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,6 +1,25 @@
-#include <Test.h>
-int main() 
+#include "Logger.h"
+#include <Hearth.h>
+class Sandbox: public HTE::Application {
+	public:
+		Sandbox() {
+
+		}
+
+		~Sandbox() {
+
+		}
+
+
+}; 
+
+HTE::Application* HTE::CreateApplication()
 {
-	HTE::Test test; 
-	test.print();
+	HTE::Logger log;
+	log.log(ERROR, "Something Terrible Happend"); 
+	log.log(INFO, "Something Terrible Happend"); 
+	log.log(DEBUG, "Something Terrible Happend"); 
+	log.log(WARN, "Something Terrible Happend"); 
+	return new Sandbox(); 
 }
+
